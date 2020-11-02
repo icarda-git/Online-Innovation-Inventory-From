9f1d76fade8a7e88a38dc550848b70e3d54cd10f
mertsincan <sincan.mert@gmail.com>
2020-11-02 10:44:44 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect, useRef } from 'react';
+import classNames from 'classnames';
+import { Route } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
+
+import PrimeReact from 'primereact/utils';
+
+import 'primereact/resources/primereact.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+import 'prismjs/themes/prism-coy.css';
+import '@fullcalendar/core/main.css';
+import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/timegrid/main.css';
+//import './layout/flags/flags.css';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div>Diamond Layout</div>
+  );
+}
+
+export default App;
